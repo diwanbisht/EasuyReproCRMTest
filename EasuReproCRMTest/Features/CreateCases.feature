2ee0fac7786f80ba662a93e730e82addff62c4e2
Diwan Bisht <diwan.bisht@gmail.com>
2022-09-10 18:14:13 +0530

    Testinging

---
+++ EasuReproCRMTest/Features/CreateCases.feature
@@ -0,0 +1,7 @@
+﻿Feature: CreateCases
+This feature is ablicable to create Cases in the CRM
+
+
+Scenario: Case Creation Test Scenario
+	Given I Navigated to 'Cases' Home screen
+	When I click button 'New Case'
---
+++ EasuReproCRMTest/StepDefiners.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Dynamics365.UIAutomation.Api.UCI;
+using OpenQA.Selenium;
+
+namespace EasyReproCRM
+{
+
+    /// <summary>
+    /// Base class for defining step bindings.
+    /// </summary>
+    public abstract class StepDefiners
+    {
+        private static WebClient? client;
+        private static XrmApp? xrmApp;
+
+
+        /// <summary>
+        /// Gets the EasyRepro WebClient.
+        /// </summary>
+        public static WebClient Client
+        {
+            get
+            {
+                if (client == null)
+                {
+                    client = new WebClient(TestSettings.Options);
+                }
+                return client;
+            }
+        }
+
+        /// <summary>
+        /// Gets the EasyRepro XrmApp.
+        /// </summary>
+        public static XrmApp XrmApp => xrmApp ?? (xrmApp = new XrmApp(Client));
+
+        /// <summary>
+        /// Gets the Selenium WebDriver.
+        /// </summary>
+        protected static IWebDriver Driver => Client.Browser.Driver;
+
+
+        protected static void Quit()
+        {
+            var driver = client?.Browser?.Driver;
+
+            xrmApp?.Dispose();
+
+            // Ensuring that the driver gets disposed. Previously we were left with orphan processes and were unable to clean up profile folders.
+            driver?.Dispose();
+
+            xrmApp = null;
+            client = null;
+        }
+
+
+    }
+}
